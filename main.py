3d265f72d152c25ebdefa2c5f329ddb2559fbd37
Jakob Eriksson <github@aurorasystems.eu>
2015-03-28 18:30:30 +0100

    Now with proxy server for much faster scraping.

--- main.py
+++ main.py
@@ -1,12 +1,57 @@
+#!/usr/local/bin/python
+# vim: set fileencoding=utf8 :
 from __future__ import print_function
 import datetime
 import scraper
 import pytz
+import sys
+
+reload(sys)
+sys.setdefaultencoding('UTF-8')
+
+
+stockholm=pytz.timezone('Europe/Stockholm')
+
+kws = ['cyberkrim',
+    'hets mot folkgrupp',
+    'itbrott',
+    'IT-brott',
+    'cybercrime',
+    'cyberbrott',
+    'cyberbrottslighet',
+    'IT-brottslighet',
+    'ITbrottslighet',
+    'IT-relaterade brott',
+    'IT-relaterad brottslighet',
+    'dataintrång',
+    'datorbedrägeri',
+    'datorintrång',
+    'barnpornografibrott',
+    'näthat',
+    'bedrägeri',
+    'gromning',
+    'grooming',
+    'phishing',
+    'skimming',
+    'skimmning',
+    'hacking',
+    'hackning',
+    'intrång',
+    'trojan',
+    'cracking',
+    'cyberterrorism',
+    'pirater',
+    'förtal']
+
+sh = ['itbrott', 'IT-relaterad brottslighet']
 
 sc = scraper.Scraper()
 
-stockholm=pytz.timezone('Europe/Stockholm')
-print(sc.search_keywords(['cyberbrott'],
-                          before=datetime.datetime(2015, 1, 1, 0, 0, tzinfo=stockholm),
-                          after=datetime.datetime(2013, 1, 1, 0, 0, tzinfo=stockholm))    )
+report = sc.search_keywords(kws,
+                            before=datetime.datetime(2015, 1, 1, 0, 0, tzinfo=stockholm),
+                            after=datetime.datetime(2013, 1, 1, 0, 0, tzinfo=stockholm))
+
+f = open('report.html', 'w')
+f.write(report)
+f.close()
 
