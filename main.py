26ec77006e62e17db2e61dd524881327fef9e9bd
Jakob Eriksson <github@aurorasystems.eu>
2015-03-30 00:54:48 +0200

    Rudimentary Excel support

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import datetime
 import scraper
 import pytz
 import sys
+import subprocess
 
 reload(sys)
 sys.setdefaultencoding('UTF-8')
@@ -45,13 +46,27 @@ kws = ['cyberkrim',
 
 shortlist = ['itbrott', 'IT-relaterad brottslighet']
 
-sc = scraper.Scraper(publication='aftonbladet.se')
 
-report = sc.search_keywords(shortlist,
+if len(sys.argv) < 2:
+    sys.stderr.write('Usage: ' + sys.argv[0] + ' <site to scrape>\n')
+    sys.exit(1)
+
+publication = sys.argv[1]
+
+sc = scraper.Scraper(publication=publication)
+
+report = sc.generate_report(shortlist,
                             before=datetime.datetime(2015, 1, 1, 0, 0, tzinfo=stockholm),
                             after=datetime.datetime(2013, 1, 1, 0, 0, tzinfo=stockholm))
 
-f = open('report.html', 'w')
+filename = publication + '.html'
+f = open(filename, 'w')
 f.write(report)
 f.close()
+
+
+subprocess.call(['wkhtmltopdf', filename, filename.replace('.html', '.pdf')])
+pdf_filename = filename.replace('.html', '.pdf')
+excel_filename = filename.replace('.html', '.xlsx')
+subprocess.call(['open', excel_filename])
 
