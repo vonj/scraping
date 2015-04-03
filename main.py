917b510555a61e81df9ac8132b1304ffef63b587
Jakob Eriksson <github@aurorasystems.eu>
2015-04-03 13:19:57 +0200

    Modularize - prepare for IDG scraping. Put report in a separate directory.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import datetime
 import scraper
 import pytz
 import sys
-import subprocess
 
 reload(sys)
 sys.setdefaultencoding('UTF-8')
@@ -55,26 +54,15 @@ kws = [
 sh = ['itbrott', 'IT-relaterad brottslighet']
 
 
-if len(sys.argv) < 2:
-    sys.stderr.write('Usage: ' + sys.argv[0] + ' <site to scrape>\n')
+if len(sys.argv) < 1:
+    sys.stderr.write('Usage: ' + sys.argv[0])
     sys.exit(1)
 
-publication = sys.argv[1]
+sc = scraper.Scraper()
 
-sc = scraper.Scraper(publication=publication)
+before=datetime.datetime(2015, 1, 1, 0, 0, tzinfo=stockholm)
+after=datetime.datetime(2013, 1, 1, 0, 0, tzinfo=stockholm)
 
-report = sc.generate_report(sh,
-                            before=datetime.datetime(2015, 1, 1, 0, 0, tzinfo=stockholm),
-                            after=datetime.datetime(2013, 1, 1, 0, 0, tzinfo=stockholm))
+report = sc.generate_reports(sh, before=before, after=after)
 
-filename = publication + '.html'
-f = open(filename, 'w')
-f.write(report)
-f.close()
-
-
-subprocess.call(['wkhtmltopdf', filename, filename.replace('.html', '.pdf')])
-pdf_filename = filename.replace('.html', '.pdf')
-excel_filename = filename.replace('.html', '.xlsx')
-subprocess.call(['open', excel_filename])
 
--- scraper.py
+++ scraper.py
@@ -12,30 +12,21 @@ import dateutil.parser
 import datetime
 import pytz
 import time
+import os
 import cachecontrol
 import html2text
 import xlsxwriter
 import savReaderWriter
+import shutil
+import subprocess
 
 
 
 class Scraper(object):
-    def __init__(self, grace=0, publication=''):
-        self._publication = publication
-        if publication == 'aftonbladet.se':
-            self._search_keyword = self._search_keyword_aftonbladet
-            self._urlbase = 'http://sok.aftonbladet.se/?sortBy=pubDate&q='
-        elif publication == 'idg.se':
-            self._search_keyword = self._search_keyword_idg
-        else:
-            raise ValueError('Publication "' +
-                             publication +
-                             '" not supported for scraping')
-
+    def __init__(self, grace=0):
         self._proxies = {
             'http': 'http://127.0.0.1:3128'
         }
-        self._publication = publication
         self._html2text = html2text.HTML2Text()
         self._html2text.ignore_links = True
         self._html2text.ignore_images = True
@@ -77,20 +68,26 @@ class Scraper(object):
         sess = requests.session()
         self._cached_sess = cachecontrol.CacheControl(sess)
 
+    def _search_keyword_idg(self, keyword, before, after):
+        print('IDG stub search for ' + keyword)
+        pass
+
     def _search_keyword_aftonbladet(self, keyword, before, after):
+        urlbase = 'http://sok.aftonbladet.se/?sortBy=pubDate&q='
         index = 0
         we_may_still_find_what_we_are_looking_for = True
 
         while we_may_still_find_what_we_are_looking_for:
-            url = self._urlbase + keyword + '&start=' + str(index)
+            url = urlbase + keyword + '&start=' + str(index)
             print(url)
             try:
-                r = self._cached_sess.get(
-                    self._urlbase +
-                    keyword +
-                    '&start=' +
-                    str(index),
-                    proxies=self._proxies)
+                r = self._cached_sess.get(url, proxies=self._proxies)
+#                r = self._cached_sess.get(
+#                    url +
+#                    keyword +
+#                    '&start=' +
+#                    str(index),
+#                    proxies=self._proxies)
             except requests.exceptions.ConnectionError as e:
                 print(e)
                 time.sleep(60)
@@ -129,16 +126,42 @@ class Scraper(object):
                     if created >= after and created < before:
                         title = link.contents[0].encode('utf-8').strip()
                         url = link.get('href').strip()
-                        self._get_article(url, title, created, updated, keyword)
-                        # Step out of loop, so we can restart search on next index...
+                        self._get_article_aftonbladet(url, title, created, updated, keyword)
                         time.sleep(self._grace) # Sleep to not hammer the web server - be polite
+                        # Step out of loop, so we can restart search on next index...
                         break
             index += 1
 
     def _render_email(self, email):
         return '<a href="mailto:' + email + '">' + email + '</a>'
 
-    def generate_report(self, keywords, before, after):
+    def generate_reports(self, keywords, before, after):
+        reportbase = 'keywords_aftonbladet_idg_' + after.strftime('%Y-%m-%d') + '-' + before.strftime('%Y-%m-%d')
+
+        try:
+            shutil.rmtree(reportbase)
+        except OSError:
+            pass
+
+        os.mkdir(reportbase)
+
+        self._reportname = os.path.join(reportbase, reportbase)
+        
+        f = open(self._reportname + '.html', 'w')
+
+        self._rownames = [
+            'idx',
+            'fetched',
+            'keywords',
+            'publication',
+            'date',
+            'updated',
+            'author',
+            'author_email',
+            'url',
+            'title',
+            'fulltext_plain',
+        ]
         spss_types = {
             'idx': 0,
             'fetched': 34,
@@ -153,168 +176,166 @@ class Scraper(object):
             'fulltext_plain': 10000,
         }
 
-        rownames = [
-            'idx',
-            'fetched',
-            'keywords',
-            'publication',
-            'date',
-            'updated',
-            'author',
-            'author_email',
-            'url',
-            'title',
-            'fulltext_plain',
-        ]
-
         with savReaderWriter.SavWriter(
-            self._publication + '.sav',
-            rownames,
+            self._reportname + '.sav',
+            self._rownames,
             spss_types,
             ioUtf8=True,
-        ) as SPSSwriter:
-            # Gather data
-            for keyword in keywords:
-                self._search_keyword(keyword.strip(), before, after)
+        ) as self._SPSSwriter:
+            f.write(self._generate_report(keywords, before, after))
+            f.close()
+            subprocess.call(
+                ['wkhtmltopdf',
+                self._reportname + '.html',
+                self._reportname + '.pdf']
+            )
+            subprocess.call(['open', self._reportname + '.xlsx'])
 
-            # Build Excel report
-            workbook = xlsxwriter.Workbook(self._publication + '.xlsx')
-            fmt = workbook.add_format({'bold': True, 'font_name': 'Verdana'})
-            sheet = workbook.add_worksheet('Data')
-            col = 0
-            for rowname in rownames:
-                sheet.write(0, col, rowname, fmt)
-                col += 1
+    def _generate_report(self, keywords, before, after):
+        # Gather data
+        for keyword in keywords:
+            keyword = keyword.strip()
+            self._search_keyword_aftonbladet(keyword, before, after)
+            self._search_keyword_idg(keyword, before, after)
 
-            # Build HTML report
-            report = '''
-    <html>
-     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-      <head>
-       <style>
-        body {
-          line-height: 1.0;
-        }
-       </style>
-      </head>
-      <body>
-       Sökning på ''' + self._publication + ' från ' + \
-        str(after.date()) + ' till ' + str(before.date()) + \
-    ''', med nyckelord enligt tabellen:
-       <table BORDER="1" RULES=ALL FRAME=VOID CELLPADDING="10">
-        <tr>
-         <th>Nyckelord</th>
-         <th>Matchande länkar</th>
-        </tr>
-    '''
+        # Build Excel report
+        workbook = xlsxwriter.Workbook(self._reportname + '.xlsx')
+        fmt = workbook.add_format({'bold': True, 'font_name': 'Verdana'})
+        sheet = workbook.add_worksheet('Data')
+        col = 0
+        for rowname in self._rownames:
+            sheet.write(0, col, rowname, fmt)
+            col += 1
 
-            for keyword, props in self._keywords.items():
-                report += \
-                    '<tr>' + \
-                    '<td><small>' + keyword + '</small></td>' + \
-                    '<td>'
-                for url in props['url']:
-                    report += ' <small><a href="' + url + '">' + url + '</a></small> '
-                report += \
-                    '</td>' + \
-                    '</tr>'
+        # Build HTML report
+        report = '''
+<html>
+ <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <head>
+   <style>
+    body {
+      line-height: 1.0;
+    }
+   </style>
+  </head>
+  <body>
+   Artikelsökning på Aftonbladet.se och IDG.se från ''' + \
+    str(after.date()) + ' till ' + str(before.date()) + \
+''', med nyckelord enligt tabellen:
+   <table BORDER="1" RULES=ALL FRAME=VOID CELLPADDING="10">
+    <tr>
+     <th>Nyckelord</th>
+     <th>Matchande länkar</th>
+    </tr>
+'''
+
+        for keyword, props in self._keywords.items():
+            report += \
+                '<tr>' + \
+                '<td><small>' + keyword + '</small></td>' + \
+                '<td>'
+            for url in props['url']:
+                report += ' <small><a href="' + url + '">' + url + '</a></small> '
+            report += \
+                '</td>' + \
+                '</tr>'
+
+        report += \
+            '</table>' + \
+            '<p style="page-break-before: always" />'
+
+        sheet.set_column(0, 0, 10)
+        sheet.set_column(1, 1, 29)
+        sheet.set_column(2, 2, 30)
+        sheet.set_column(3, 3, 12)
+        sheet.set_column(4, 4, 22)
+        sheet.set_column(5, 5, 22)
+        sheet.set_column(6, 6, 21)
+        sheet.set_column(7, 7, 35)
+        sheet.set_column(8, 8, 70)
+        sheet.set_column(9, 9, 70)
+        sheet.set_column(10, 10, 240)
+
+        row = 0
+        for _key, a in sorted(self._articles.items()):
+
+            fetched = a['fetched'].isoformat()
+            keywords = ', '.join(a['keywords'])
+            publication = a['publication']
+            created = a['created'].isoformat()
+            updated = a['updated'].isoformat()
+            author = self._html2text.handle(a['author'])
+            author_email = a['author_email']
+            url = a['url']
+            title = a['title'].replace('\n', ' ')
+            fulltext_plain = a['fulltext_plain'].replace('\n', ' ')
+
+            row += 1
+            sheet.write(row,  0, row)
+            sheet.write(row,  1, fetched)
+            sheet.write(row,  2, keywords)
+            sheet.write(row,  3, a['publication'])
+            sheet.write(row,  4, created)
+            sheet.write(row,  5, updated)
+            sheet.write(row,  6, author)
+            sheet.write(row,  7, author_email)
+            sheet.write(row,  8, url)
+            sheet.write(row,  9, title)
+            sheet.write(row, 10, fulltext_plain)
+
+            self._SPSSwriter.writerow([
+                row,
+                fetched,
+                keywords,
+                a['publication'],
+                created,
+                updated,
+                author,
+                author_email,
+                url,
+                title,
+                fulltext_plain,
+            ])
 
             report += \
-                '</table>' + \
-                '<p style="page-break-before: always" />'
+            '<table CELLPADDING=6 RULES=GROUPS  FRAME=BOX>' + \
+            '<tr>' + \
+            '<td>Titel:</td>' + \
+            '<td><b>' + a['title'] + '</b></td>' + \
+            '</tr>' + \
+            '<tr>' + \
+            '<td>Skapad:</td>' + \
+            '<td>' + self._dstr(a['created']) + '</td>' + \
+            '</tr>' + \
+            '<tr>' + \
+            '<td>Senast uppdaterad:</td>' + \
+            '<td>' + self._dstr(a['updated']) + '</td>' + \
+            '</tr>' + \
+            '<tr>' + \
+            '<td>Källa:</td>' + \
+            '<td><i><a href="' + a['url'] + '">' + a['url'] + '</a></i></td>' + \
+            '</tr>' + \
+            '<tr>' + \
+            '<td>Hämtad:</td>' + \
+            '<td>' + self._dstr(a['fetched']) + ' </td>' + \
+            '</tr>' + \
+            '<tr>' + \
+            '<td>Nyckelord:</td>' + \
+            '<td>' + keywords + ' </td>' + \
+            '</table>' + \
+            a['lead'] + \
+            a['body'] + \
+            a['author'] + \
+            self._render_email(a['author_email']) + \
+            '<p style="page-break-before: always" />'
 
-            sheet.set_column(0, 0, 10)
-            sheet.set_column(1, 1, 29)
-            sheet.set_column(2, 2, 30)
-            sheet.set_column(3, 3, 12)
-            sheet.set_column(4, 4, 22)
-            sheet.set_column(5, 5, 22)
-            sheet.set_column(6, 6, 21)
-            sheet.set_column(7, 7, 35)
-            sheet.set_column(8, 8, 70)
-            sheet.set_column(9, 9, 70)
-            sheet.set_column(10, 10, 240)
+        report += \
+            '</body>' + \
+            '</html>'
 
-            row = 0
-            for _key, a in self._articles.items():
+        report_text, errors = tidylib.tidy_document(report)
 
-                fetched = a['fetched'].isoformat()
-                keywords = ', '.join(a['keywords'])
-                publication = self._publication
-                created = a['created'].isoformat()
-                updated = a['updated'].isoformat()
-                author = self._html2text.handle(a['author'])
-                author_email = a['author_email']
-                url = a['url']
-                title = a['title'].replace('\n', ' ')
-                fulltext_plain = a['fulltext_plain'].replace('\n', ' ')
-
-                row += 1
-                sheet.write(row,  0, row)
-                sheet.write(row,  1, fetched)
-                sheet.write(row,  2, keywords)
-                sheet.write(row,  3, self._publication)
-                sheet.write(row,  4, created)
-                sheet.write(row,  5, updated)
-                sheet.write(row,  6, author)
-                sheet.write(row,  7, author_email)
-                sheet.write(row,  8, url)
-                sheet.write(row,  9, title)
-                sheet.write(row, 10, fulltext_plain)
-
-                SPSSwriter.writerow([
-                    row,
-                    fetched,
-                    keywords,
-                    self._publication,
-                    created,
-                    updated,
-                    author,
-                    author_email,
-                    url,
-                    title,
-                    fulltext_plain,
-                ])
-
-                report += \
-                '<table CELLPADDING=6 RULES=GROUPS  FRAME=BOX>' + \
-                '<tr>' + \
-                '<td>Titel:</td>' + \
-                '<td><b>' + a['title'] + '</b></td>' + \
-                '</tr>' + \
-                '<tr>' + \
-                '<td>Skapad:</td>' + \
-                '<td>' + self._dstr(a['created']) + '</td>' + \
-                '</tr>' + \
-                '<tr>' + \
-                '<td>Senast uppdaterad:</td>' + \
-                '<td>' + self._dstr(a['updated']) + '</td>' + \
-                '</tr>' + \
-                '<tr>' + \
-                '<td>Källa:</td>' + \
-                '<td><i><a href="' + a['url'] + '">' + a['url'] + '</a></i></td>' + \
-                '</tr>' + \
-                '<tr>' + \
-                '<td>Hämtad:</td>' + \
-                '<td>' + self._dstr(a['fetched']) + ' </td>' + \
-                '</tr>' + \
-                '<tr>' + \
-                '<td>Nyckelord:</td>' + \
-                '<td>' + keywords + ' </td>' + \
-                '</table>' + \
-                a['lead'] + \
-                a['body'] + \
-                a['author'] + \
-                self._render_email(a['author_email']) + \
-                '<p style="page-break-before: always" />'
-
-            report += \
-                '</body>' + \
-                '</html>'
-
-            report_text, errors = tidylib.tidy_document(report)
-
-            workbook.close()
+        workbook.close()
 
         return report_text
 
@@ -356,7 +377,7 @@ class Scraper(object):
             return l[0]
         return ''
 
-    def _get_article(self, url, title, created, updated, keyword):
+    def _get_article_aftonbladet(self, url, title, created, updated, keyword):
         request_done = False
         while not request_done:
             try:
